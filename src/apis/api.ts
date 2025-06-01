b688cfef0344cfb23303658cb2ef2b296905f291
Arvin Hasim <arvin.hasim@binus.ac.id>
2025-06-01 20:58:21 +0700

    #fix adjust api call url

--- src/apis/api.ts
+++ src/apis/api.ts
@@ -1,7 +1,9 @@
 // Local API
 const api = {
-  BaseUrl: "http://localhost:8000",
-  BaseUrlAPI: "http://localhost:8000/api",
+  // BaseUrl: "http://localhost:8000",
+  BaseUrl: "https://promigoapi-production.up.railway.app",
+  // BaseUrlAPI: "http://localhost:8000/api",
+  BaseUrlAPI: "https://promigoapi-production.up.railway.app/api",
   baseCloudPath:
     "https://wfovnuzxqrgmlgcrahdm.supabase.co/storage/v1/object/public/promigocloud/",
 }; 
