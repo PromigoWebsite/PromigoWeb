b2009e81007add6c40e4b8e7e7248db014ff5073
Arvin Hasim <arvin.hasim@binus.ac.id>
2025-06-04 04:26:31 +0700

    fix: Update API configuration to use production URLs

--- src/apis/api.ts
+++ src/apis/api.ts
@@ -1,18 +1,18 @@
 // Local api
-const api = {
-  BaseUrl: "http://localhost:8000",
-  BaseUrlAPI: "http://localhost:8000/api",
-  baseCloudPath:
-    "https://wfovnuzxqrgmlgcrahdm.supabase.co/storage/v1/object/public/promigocloud/",
-}; 
-
-//prod API
 // const api = {
-//   BaseUrl: "https://promigoapi-production.up.railway.app",
-//   BaseUrlAPI: "https://promigoapi-production.up.railway.app/api",
+//   BaseUrl: "http://localhost:8000",
+//   BaseUrlAPI: "http://localhost:8000/api",
 //   baseCloudPath:
 //     "https://wfovnuzxqrgmlgcrahdm.supabase.co/storage/v1/object/public/promigocloud/",
 // }; 
+
+//prod API
+const api = {
+  BaseUrl: "https://promigoapi-production.up.railway.app",
+  BaseUrlAPI: "https://promigoapi-production.up.railway.app/api",
+  baseCloudPath:
+    "https://wfovnuzxqrgmlgcrahdm.supabase.co/storage/v1/object/public/promigocloud/",
+}; 
 
 
 export default api;
